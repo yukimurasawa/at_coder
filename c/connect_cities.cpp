fa5e7582da445fc031289a6bb699c339a90782c0
yukimurasawa <yuki.murasawa0826@gmail.com>
2020-09-27 13:34:37 +0900

    9/27

--- c/connect_cities.cpp
+++ c/connect_cities.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 
 int main()
 {
   int n, m;
   cin >> n >> m;
-  vector<int> a(m);
+  dsu d(n);
+
   for (int i = 0; i < m; i++)
   {
-    cin >> a[i];
+    int a, b;
+    cin >> a >> b;
+    --a;
+    --b;
+    d.merge(a, b);
   }
+
+  int cnt = d.groups().size();
+  int ans = cnt - 1;
+  cout << ans << endl;
 
   return 0;
 }
